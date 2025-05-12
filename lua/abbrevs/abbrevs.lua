fed8ec87ddb5fecb262a3f0c0e4bcc8acc5a55ee
TinyProbe <137adv167@gmail.com>
2025-05-12 17:08:13 +0900

    Update Changes

--- lua/abbrevs/abbrevs.lua
+++ lua/abbrevs/abbrevs.lua
@@ -1,7 +1,19 @@
 -- html
-vim.api.nvim_command('command AbbrevHtmlBasic :read ~/.config/vim/abbrevs/html/basic.html')
+vim.api.nvim_create_user_command(
+  "AbbrevHtmlBasic",
+  "read ~/.config/vim/abbrevs/html/basic.html", {}
+)
 
 -- dart
-vim.api.nvim_command('command AbbrevDartBasic :read ~/.config/vim/abbrevs/dart/basic.dart')
-vim.api.nvim_command('command AbbrevDartStateful :read ~/.config/vim/abbrevs/dart/stateful.dart')
-vim.api.nvim_command('command AbbrevDartStateless :read ~/.config/vim/abbrevs/dart/stateless.dart')
+vim.api.nvim_create_user_command(
+  "AbbrevDartBasic",
+  "read ~/.config/vim/abbrevs/dart/basic.dart", {}
+)
+vim.api.nvim_create_user_command(
+  "AbbrevDartStateful",
+  "read ~/.config/vim/abbrevs/dart/stateful.dart", {}
+)
+vim.api.nvim_create_user_command(
+  "AbbrevDartStateless",
+  "read ~/.config/vim/abbrevs/dart/stateless.dart", {}
+)
