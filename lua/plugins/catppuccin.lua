d83f010bbd9b5d00a313ca9978fe987d3f4e0186
TinyProbe <137adv167@gmail.com>
2025-07-09 23:49:02 +0900

    Update Changes

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,12 +3,12 @@ return {
   priority = 1000,
   config = function()
     require("catppuccin").setup({
-      flavour = "mocha",  -- latte, frappe, macchiato, mocha
+      -- flavour = "mocha",  -- latte, frappe, macchiato, mocha
       background = {
         light = "latte",    -- :help background
-        dark = "mocha",
+        dark = "macchiato",
       },
-      transparent_background = true, -- disables setting the background color.
+      transparent_background = false, -- disables setting the background color.
       show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
       term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
       dim_inactive = {
